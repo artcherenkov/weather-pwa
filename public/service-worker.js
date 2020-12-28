6135d036592fe76572d75cb26c8e1f9269ae62e3
Artem Cherenkov <art.cherenkov@gmail.com>
2020-12-29 00:08:15 +0700

    fixes

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,9 +2,6 @@ const CACHE_PREFIX = `weather-cache`;
 const CACHE_VER = `v1`;
 const CACHE_NAME = `${CACHE_PREFIX}-${CACHE_VER}`;
 
-const HTTP_STATUS_OK = 200;
-const RESPONSE_SAFE_TYPE = `basic`;
-
 const self = this;
 
 self.addEventListener(`install`, (evt) => {
@@ -12,9 +9,9 @@ self.addEventListener(`install`, (evt) => {
     caches.open(CACHE_NAME)
       .then((cache) => {
         return cache.addAll([
-          `/`,
-          `/index.html`,
-          `/images/logo.png`
+          `index.html`,
+          `images/logo.png`,
+          'offline.html'
         ]);
       })
   );
@@ -23,19 +20,14 @@ self.addEventListener(`install`, (evt) => {
 self.addEventListener(`activate`, (evt) => {
   evt.waitUntil(
     caches.keys()
-      .then(
-        (keys) => Promise.all(
-          keys.map(
-            (key) => {
-              if (key.startsWith(CACHE_PREFIX) && key !== CACHE_NAME) {
-                return caches.delete(key);
-              }
-
-              return null;
-            })
-            .filter((key) => key !== null)
-        )
-      )
+      .then((keys) => Promise.all(keys.map((key) => {
+          if (key.startsWith(CACHE_PREFIX) && key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+          return null;
+        })
+          .filter((key) => key !== null)
+      ))
   );
 });
 
@@ -44,23 +36,9 @@ const handleFetch = (evt) => {
 
   evt.respondWith(
     caches.match(request)
-      .then((cacheResponse) => {
-        if (cacheResponse) {
-          return cacheResponse;
-        }
-
-        return fetch(request)
-          .then((response) => {
-            if (!response || response.status !== HTTP_STATUS_OK || response.type !== RESPONSE_SAFE_TYPE) {
-              return response;
-            }
-
-            const clonedResponse = response.clone();
-            caches.open(CACHE_NAME)
-              .then((cache) => cache.put(request, clonedResponse));
-
-            return response;
-          });
+      .then(() => {
+        return fetch(evt.request)
+          .catch(() => caches.match('offline.html'))
       })
   );
 };
